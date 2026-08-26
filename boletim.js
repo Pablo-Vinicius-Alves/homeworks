56bdd84d05efd02bc6a099c67f05daa64ed38cc2
Pablo Vinícius Alves Pisolato <pblalves29@gmail.com>
2026-08-26 20:33:21 -0300

    Add objects

--- boletim.js
+++ boletim.js
@@ -3,24 +3,25 @@ let professor = true;
 console.log("Se você for um professor, insira o número do seu crachá.");
 console.log("-----------------------------------------------");
 
-const nomeAlunos = ["Marcos", "Jefersson", "Erica"];
+let nomeAlunos = {
+    nome: ["Marcos", "Jefersson", "Erica"],
+    idade: [15, 16, 16]
+};
 
-let verAluno = 1;
 let posicao = -1;
 
-function escolherAluno(){
-    for(nome of nomeAlunos){
-        posicao ++;
-    console.log(posicao, nome);
+function escolherAluno() {
+    for (nome of nomeAlunos.nome) {
+        posicao++;
+        console.log(posicao, nome,);
     }
-    
 }
 
-let alunoNotas = [4, 7, 8, 7, 9]; 
- 
+let alunoNotas = [4, 7, 8, 7, 9];
+
 let somaArray = 0;
-function somaMedia(){
-    for (let i = 0; i < alunoNotas.length; i++){
+function somaMedia() {
+    for (let i = 0; i < alunoNotas.length; i++) {
         console.log(alunoNotas[i]);
         somaArray += alunoNotas[i];
     }
@@ -28,9 +29,9 @@ function somaMedia(){
 
 let mediaTotal = 0;
 
-function calculoMedia(){
+function calculoMedia() {
     mediaTotal = (somaArray / alunoNotas.length);
-    if (mediaTotal >= 7){
+    if (mediaTotal >= 7) {
         console.log(`Sua média final é: ${mediaTotal} - Aprovado`);
     }
     else {
@@ -40,36 +41,36 @@ function calculoMedia(){
 
 let contador = 0;
 
-function quantMedia(){
-    for(let i = 0; i < alunoNotas.length; i++){
-        if (alunoNotas[i] >= mediaTotal){
-            contador ++;
-        } 
+function quantMedia() {
+    for (let i = 0; i < alunoNotas.length; i++) {
+        if (alunoNotas[i] >= mediaTotal) {
+            contador++;
+        }
     }
 }
 
 let maiorNota = alunoNotas[0];
 let menorNota = alunoNotas[0];
 
-function maiorMenor(){
-    for(let i = 0; i < alunoNotas.length; i++){
-        if (alunoNotas[i] > maiorNota){
+function maiorMenor() {
+    for (let i = 0; i < alunoNotas.length; i++) {
+        if (alunoNotas[i] > maiorNota) {
             maiorNota = alunoNotas[i];
         }
-        if(alunoNotas[i] < menorNota){
+        if (alunoNotas[i] < menorNota) {
             menorNota = alunoNotas[i];
         }
     }
 }
 
-while (professor == true){
+while (professor == true) {
     escolherAluno();
-    let escolhaAluno = nomeAlunos[1];
+    let escolhaAluno = "Marcos"
     console.log(`Qual aluno deseja entar no boletim? - ${escolhaAluno}`);
     console.log("------------------------------------------------");
     console.log(`As notas do ${escolhaAluno} foram:`);
     somaMedia();
-    console.log(somaArray);
+    console.log(`Soma - ${somaArray}`);
     calculoMedia();
     quantMedia();
     console.log(`O aluno ficou com ${contador} notas acima da média.`);
