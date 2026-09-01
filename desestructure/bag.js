56ff6b67bf458d6973c3c0e65d9c9cf3f1bdf64a
Pablo Alves <pblalves29@gmail.com>
2026-09-01 15:59:54 -0300

    desestructure

---
+++ desestructure/bag.js
@@ -0,0 +1,25 @@
+function main() {
+
+    const player = {
+        nickname: "Thorfinn",
+        health: 50,
+        inventory: {
+            itens: ["Sword", "Shield", "Spear"],
+            potions: [
+                { type: "regeneration", duration: 10 },
+                { type: "strength", duration: 20 },
+            ]
+        },
+    }
+
+    const { inventory } = player
+    const { itens } = inventory
+    console.log(inventory);
+    const { inventory: { potions } } = player
+
+    const [{ type, duration }, { type: secondType, duration: secondDuration }] = potions
+    console.log(type, duration);
+    console.log(secondType, secondDuration);
+}
+
+main();
