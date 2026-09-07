c26d65115ba391bd3297e45a95223501f8309e0f
Pablo Vinícius Alves Pisolato <pblalves29@gmail.com>
2026-09-07 19:59:27 -0300

    library pt2

--- bibliotecas/index.js
+++ bibliotecas/index.js
@@ -1,19 +1,23 @@
 import chalk from "chalk";
-import { intro, text } from "@clack/prompts"
+import { intro, password } from "@clack/prompts";
 
 const senha = "Pamonha999";
 
 async function Login() {
-    console.log(chalk.redBright.underline(`Login`));
-    const correct = await text({ message: "Insira sua senha:" });
-    if (correct === senha) {
-        intro(chalk.bgGreen(" SUCESSO! "));
-    } else {
+    for (let i = 0; i < 3; i++) {
+        console.log(chalk.redBright.underline(`Login`));
+        const correct = await password({ message: "Insira sua senha:" });
 
-        intro(chalk.bgRed(" Dados inccorretos! "))
-        return Login();
+        if (correct === senha) {
+            intro(chalk.bgGreen(" SUCESSO! "));
+            return;
+        } else {
+            intro(chalk.bgRed(" Dados incorretos! "));
+        }
     }
+    intro(chalk.bgRed(" Limite de tentativas excedido. "));
 }
 
 Login();
+
 
